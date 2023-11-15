2d3c1c28345246f113467224df6c7a912196746d
Andrzej Gebus <andrzejgebuss@gmail.com>
2023-11-15 21:39:19 +0100

    init test

---
+++ android/app/src/main/kotlin/com/example/olx_bot/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.olx_bot
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:background_fetch/background_fetch.dart';
+import 'package:flutter/material.dart';
+import 'package:olx_bot/backgroundServices/olx_service.dart';
+import 'package:olx_bot/src/service_page.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(const App());
+
+  BackgroundFetch.registerHeadlessTask(backgroundFetchHeadlessTask);
+}
+
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      // home: const HomePage(title: 'Flutter Demo Home Page'),
+      home: ServiceTest(),
+    );
+  }
+}
