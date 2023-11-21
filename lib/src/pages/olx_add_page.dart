b93fde73accbd258a46221b805db769b76d7d5d7
Andrzej Gebus <andrzejgebuss@gmail.com>
2023-11-21 16:58:51 +0100

    wip

--- lib/src/pages/olx_add_page.dart
+++ lib/src/pages/olx_add_page.dart
@@ -10,34 +10,60 @@ class OlxAddPage extends StatefulWidget {
 }
 
 class _OlxAddPageState extends State<OlxAddPage> {
-  late OlxLocationSuggestion city;
+  _OlxAddPageState();
 
+  OlxLocationSuggestion? location;
   TextEditingController cityController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        children: [
-          ListTile(
-            leading: const Icon(Icons.location_city_rounded),
-            title: TextFormField(
-              controller: cityController,
-              readOnly: true,
-              onTap: () async {
-                final location = await showSearch(
-                  context: context,
-                  delegate: OlxCitySearchDelegate(),
-                );
+        appBar: AppBar(
+          title: const Text("Add new filter"),
+        ),
+        body: Form(
+            key: _formKey,
+            child: ListView(
+              children: [
+                ListTile(
+                  leading: const Icon(Icons.location_city_rounded),
+                  title: TextFormField(
+                    controller: cityController,
+                    readOnly: true,
+                    onTap: () async {
+                      final location = await showSearch(
+                        context: context,
+                        delegate: OlxCitySearchDelegate(),
+                      );
 
-                if (location != null) {
-                  cityController.text = location.city.name;
-                }
-              },
-            ),
-          ),
-        ],
-      ),
-    );
+                      if (location != null) {
+                        setState(() {
+                          this.location = location;
+                        });
+
+                        cityController.text = location.city.name;
+                      }
+                    },
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(content: Text('Processing Data')),
+                      );
+                    }
+                  },
+                  child: const Text('Submit'),
+                ),
+                if (location != null) Text('city: ${location?.city.id}'),
+                if (location != null)
+                  Text('city: ${location?.city.normalizedName}'),
+                if (location != null) Text('region: ${location?.region.id}'),
+                if (location != null)
+                  Text('region: ${location?.region.normalizedName}'),
+              ],
+            )));
   }
 }
